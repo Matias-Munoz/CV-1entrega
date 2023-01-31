7d4131e9bf173048f177bb1a904ed4283c292f27
Matias_Munoz <matiasfmunoz.93@gmail.com>
2023-01-31 08:19:41 -0300

    Inicio entrega CV

--- prueba_0/for_prueba.js
+++
@@ -1,6 +0,0 @@
-
-var i = 0;
-
-for(i; i < 10; i++) {
-    console.log("El valor de x es :"+i)
-}
--- prueba_0/while_prueba.js
+++
@@ -1,7 +0,0 @@
-
-var x = 10;
-
-while (x > 6) {
-    console.log("Mi variable vale :"+x);
-    x--;
-}
